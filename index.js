d094773c2b45cdefec71905f156abdb50605b3ad
Mohana Misra <iammohanamisra@gmail.com>
2023-12-18 21:24:47 +0530

    Completed 3.4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use(express.json());
 
-const persons = [
+let persons = [
     {
         "id": 1,
         "name": "Arto Hellas",
@@ -42,7 +42,16 @@ app.get('/info', (request, response) => {
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id);
     const person = persons.find(person => person.id === id);
-    response.json(person);
+    if(person)
+        response.json(person);
+    else
+        response.status(404).end();
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end();
 })
 
 const PORT = 3001;
