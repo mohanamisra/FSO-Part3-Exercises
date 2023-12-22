8ac34d90768475cd6e20355c679c92926974e715
Mohana Misra <iammohanamisra@gmail.com>
2023-12-22 20:09:17 +0530

    Backend working with dotenv and mongodb

--- index.js
+++ index.js
@@ -63,59 +63,59 @@ let persons = [
 
 //MONGODB CODE
 
-const mongoose = require('mongoose')
-
-if(process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1);
-}
-
-const password = process.argv[2];
-
-const url = `mongodb+srv://lordmagic:${password}@cluster0.z6jhhex.mongodb.net/?retryWrites=true&w=majority`;
-
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
-
-const personSchema = new mongoose.Schema({
-    name: String,
-    number: Number,
-});
-
-personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
-})
-
-const Person = mongoose.model('Person', personSchema);
-
-const length = process.argv.length;
-if(length > 3) {
-    const personNumber = Number(process.argv[length - 1]);
-    const personName = process.argv[3];
-
-    const person = new Person({
-        name: personName,
-        number: personNumber,
-    })
-
-    person.save().then(result => {
-        console.log(`added ${person.name} number ${person.number} to phonebook`);
-        mongoose.connection.close();
-    })
-}
-else {
-    console.log('phonebook:');
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(person);
-        })
-        mongoose.connection.close();
-    })
-}
+// const mongoose = require('mongoose')
+//
+// if(process.argv.length < 3) {
+//     console.log('give password as argument')
+//     process.exit(1);
+// }
+//
+// const password = process.argv[2];
+//
+// const url = `mongodb+srv://lordmagic:${password}@cluster0.z6jhhex.mongodb.net/?retryWrites=true&w=majority`;
+//
+// mongoose.set('strictQuery', false);
+// mongoose.connect(url);
+//
+// const personSchema = new mongoose.Schema({
+//     name: String,
+//     number: Number,
+// });
+//
+// personSchema.set('toJSON', {
+//     transform: (document, returnedObject) => {
+//         returnedObject.id = returnedObject._id.toString();
+//         delete returnedObject._id;
+//         delete returnedObject.__v;
+//     }
+// })
+//
+// const Person = mongoose.model('Person', personSchema);
+//
+// const length = process.argv.length;
+// if(length > 3) {
+//     const personNumber = Number(process.argv[length - 1]);
+//     const personName = process.argv[3];
+//
+//     const person = new Person({
+//         name: personName,
+//         number: personNumber,
+//     })
+//
+//     person.save().then(result => {
+//         console.log(`added ${person.name} number ${person.number} to phonebook`);
+//         mongoose.connection.close();
+//     })
+// }
+// else {
+//     console.log('phonebook:');
+//     Person.find({}).then(result => {
+//         result.forEach(person => {
+//             console.log(person);
+//         })
+//         mongoose.connection.close();
+//     })
+// }
 
 //  REQUEST ROUTES
 
