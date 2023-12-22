b5c18534b83efcd62bc5119c71ddcb880fa6fcb3
Mohana Misra <iammohanamisra@gmail.com>
2023-12-22 19:24:52 +0530

    Added mongoDB code to index.js

--- index.js
+++ index.js
@@ -58,6 +58,54 @@ let persons = [
     }
 ]
 
+//MONGODB CODE
+
+const mongoose = require('mongoose')
+
+if(process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://lordmagic:${password}@cluster0.z6jhhex.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: Number,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const length = process.argv.length;
+if(length > 3) {
+    const personNumber = Number(process.argv[length - 1]);
+    const personName = process.argv[3];
+
+    const person = new Person({
+        name: personName,
+        number: personNumber,
+    })
+
+    person.save().then(result => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`);
+        mongoose.connection.close();
+    })
+}
+else {
+    console.log('phonebook:');
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person);
+        })
+        mongoose.connection.close();
+    })
+}
+
 //  REQUEST ROUTES
 
 app.get('/api/persons', (request, response) => {
